2c2415e0d47604ff9e26fff6aeebc00fc36d22ca
Sandrine L'Anglais-Landry <slanglaislandrysds@gmail.com>
2023-09-13 15:39:08 -0400

    updated :fire:

--- Notes_perso_Sandrine.R
+++ Notes_perso_Sandrine.R
@@ -2,40 +2,40 @@ install.packages(c('quantmod','ff','foreign','R.matlab'),dependency=T)
 
 suppressPackageStartupMessages(library(tidyverse)) #this dont work /// might work now
 
-setwd("C:/Users/Sandrine/Documents/Automne 2023 Mattew barbour/AraDiv_Dataset") #pour moi
+
 
 install.packages("tidyverse") 
-library(tidyverse)
+library(tidyverse) #pour loader mes affaires
 
 
-README_meteorological_datarecord <- read.delim("~/Automne 2023 Mattew barbour/AraDiv_Dataset/README_meteorological_datarecord.txt")
+README_meteorological_datarecord <- read.delim("dataverse_files/README_meteorological_datarecord.txt"
 View(README_meteorological_datarecord)
 
-README_nirs_datarecord <- read.delim("~/Automne 2023 Mattew barbour/AraDiv_Dataset/README_nirs_datarecord.txt")
+README_nirs_datarecord <- read.delim("dataverse_files/README_nirs_datarecord.txt")
 View(README_nirs_datarecord)
 
-README_phenotypic_datarecord <- read.delim("~/Automne 2023 Mattew barbour/AraDiv_Dataset/README_phenotypic_datarecord.txt", comment.char="#")
+README_phenotypic_datarecord <- read.delim("dataverse_files/README_phenotypic_datarecord.txt")
 View(README_phenotypic_datarecord)
 
-meteorological_datarecord <- read_delim("~/Automne 2023 Mattew barbour/AraDiv_Dataset/meteorological_datarecord.txt")
+meteorological_datarecord <- read_delim("dataverse_files/meteorological_datarecord.txt")
+View(meteorological_datarecord)
+
+
+nirs_datarecord <- read_delim("dataverse_files/nirs_datarecord.txt")
 View(nirs_datarecord)
 
+phenotypic_datarecord <- read_delim("dataverse_files/phenotypic_datarecord.txt")
+View(phenotype_data_record) #voir le data
 
-nirs_datarecord <- read_delim("~/Automne 2023 Mattew barbour/AraDiv_Dataset/nirs_datarecord.txt")#, header=FALSE)
-View(nirs_datarecord)
-
-phenotypic_datarecord <- read_delim("~/Automne 2023 Mattew barbour/AraDiv_Dataset/phenotypic_datarecord.txt")#, header=FALSE)
-View(phenotype)
-
-str(phenotypic_datarecord)
+str(phenotypic_datarecord) #Compactly Display the Structure of an Arbitrary R Object
 
 
 Ljoined <- left_join(nirs_datarecord,phenotypic_datarecord)
 Fjoined <- full_join(nirs_datarecord,phenotypic_datarecord)
 Rjoined <- right_join(nirs_datarecord,phenotypic_datarecord)
 
-Summary(Ljoined) # Fjoin et R join aussi
-View(Ljoined) # Fjoin et R join aussi
+Summary(Ljoined) # Fjoin et R join aussi, fait un résumé
+View(Ljoined) # Fjoin et R join aussi 
 
 # moyen utile pck apres code a mattew fonctionne
 Ljoined[10000:10012,2000:2004] #  pour voir les lignes 1-12 colonne 1-4
@@ -55,8 +55,8 @@ library(tidyverse)
 # paths are specific to my computer so ou will have to change them. 
 # also, I'm using read_delim function that I showed you yesterday
 
-nirs_datarecord <- read_delim("~/Automne 2023 Mattew barbour/AraDiv_Dataset/nirs_datarecord.txt")
-phenotypic_datarecord <- read_delim("~/Automne 2023 Mattew barbour/AraDiv_Dataset/phenotypic_datarecord.txt") 
+nirs_datarecord <- read_delim("dataverse_files/nirs_datarecord.txt")
+phenotypic_datarecord <- read_delim("dataverse_files/phenotypic_datarecord.txt") 
 
 # subset phenotypic data to only use P- plants for verbatimOccurenceID
 phenotypic_sub <- phenotypic_datarecord %>%
