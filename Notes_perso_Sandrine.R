ab0cf45c94d8b4d84d806ae0dbb35a9ffdb55acf
Sandrine L'Anglais-Landry <slanglaislandrysds@gmail.com>
2023-09-21 22:34:47 -0400

    my progress so far, slow but steady, had to debug a few things but all worked ok. There is more code in my personal notes but i am still working on it and not sure it works yet

--- Notes_perso_Sandrine.R
+++ Notes_perso_Sandrine.R
@@ -79,4 +79,140 @@ phenotypic_nirs_df <- left_join(phenotypic_sub, nirs_datarecord) %>%
   drop_na()
 
 
+#-----------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------
+  
+ ##############PLSR 
+  
+  
+  source("manage_data.R")
+
+list.of.packages <- c("pls","dplyr","reshape2","here","plotrix","ggplot2","gridExtra",
+                      "spectratrait")
+invisible(lapply(list.of.packages, library, character.only = TRUE))
+
+req.packages <- c("devtools","remotes","readr","RCurl","httr","pls","dplyr","reshape2","here",
+                  "plotrix","scales","ggplot2","gridExtra")
+new.packages <- req.packages[!(req.packages %in% installed.packages()[,"Package"])]
+
+if(length(new.packages)) install.packages(new.packages, dependencies=TRUE)
+library(pls)
+library(dplyr)
+library(reshape2)
+library(here)
+library(plotrix)
+library(ggplot2)
+library(gridExtra)
+library(spectratrait)
+library(devtools)
+library(remotes)
+library(readr)
+library(RCurl)
+library(scales)
+
+
+# not in
+`%notin%` <- Negate(`%in%`)
+
+
+# Script options
+pls::pls.options(plsralg = "oscorespls")
+pls::pls.options("plsralg")
+
+
+# Default par options
+opar <- par(no.readonly = T)
+
+# What is the target variable?
+inVar <- "classification_name"
+
+# faire ca pour trouver les dernières colonnes
+full_df[1:5,2160:2166] # ligne 1-12 colonne 1-4
+
+#---------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------
+
+
+#renaming all the column names of data frame
+
+range <- 350:2500
+
+colnames(full_df2)[6:2156] <- c(range)
+
+### Create plsr dataset
+Start.wave <- 500 #(entre 500 et 2400nm )
+End.wave <- 2400
+wv <- seq(Start.wave,End.wave,1)
+Spectra <- as.matrix(full_df[,names(full_df) %in% wv])
+
+colnames(Spectra) <- c(paste0("Wave_",wv))
+head(Spectra)[1:6,1:10]
+sample_info <- full_df[,names(full_df) %notin% seq(350,2500,1)] # on a oter les wavelengt 350
+head(sample_info)
+
+
+sample_info2 <- sample_info %>%
+  select(X1001g_ID,AOP_status,Classification_name)
+head(sample_info2)
+
+plsr_data <- data.frame(sample_info2,Spectra) #join le sample et spectra
+
+# rm(sample_info,sample_info2,Spectra) (((pas encore sure de ca)))
+
+#------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------
+
+#### Example data cleaning.  End user needs to do what's appropriate for their 
+#### data.  This may be an iterative process.
+# Keep only complete rows of inVar and spec data before fitting
+
+plsr_data <- plsr_data[complete.cases(plsr_data[,names(plsr_data) %in% 
+                                                  c(inVar,paste0("Wave_",wv))]),] 
+
+
+#--------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------#
+### Create cal/val datasets
+## Make a stratified random sampling in the strata USDA_Species_Code and Domain
+
+method <- "dplyr" #base/dplyr
+# base R - a bit slow
+# dplyr - much faster
+
+split_data <- spectratrait::create_data_split(dataset=plsr_data, approach=method, 
+                                              split_seed=7529075,prop=0.8, 
+                                              group_variables="Species_Code")
+names(split_data)
+cal.plsr.data <- split_data$cal_data
+head(cal.plsr.data)[1:8]
+val.plsr.data <- split_data$val_data
+head(val.plsr.data)[1:8]
+rm(split_data) 
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
 
--- PLSR_analysis.R
+++ PLSR_analysis.R
@@ -3,3 +3,91 @@
 
 # source in data
 source("manage_data.R")
+
+list.of.packages <- c("pls","dplyr","reshape2","here","plotrix","ggplot2","gridExtra",
+                      "spectratrait")
+invisible(lapply(list.of.packages, library, character.only = TRUE))
+
+req.packages <- c("devtools","remotes","readr","RCurl","httr","pls","dplyr","reshape2","here",
+                  "plotrix","scales","ggplot2","gridExtra")
+new.packages <- req.packages[!(req.packages %in% installed.packages()[,"Package"])]
+
+if(length(new.packages)) install.packages(new.packages, dependencies=TRUE)
+library(pls)
+library(dplyr)
+library(reshape2)
+library(here)
+library(plotrix)
+library(ggplot2)
+library(gridExtra)
+library(spectratrait)
+library(devtools)
+library(remotes)
+library(readr)
+library(RCurl)
+library(scales)
+
+
+# not in
+`%notin%` <- Negate(`%in%`)
+
+
+# Script options
+pls::pls.options(plsralg = "oscorespls")
+pls::pls.options("plsralg")
+
+
+# Default par options
+opar <- par(no.readonly = T)
+
+# What is the target variable?
+inVar <- "classification_name"
+
+#---------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------
+
+#renaming all the column names of data frame
+
+range <- 350:2500
+
+colnames(full_df)[6:2156] <- c(range)
+
+
+### Create plsr dataset
+
+Start.wave <- 500 #(entre 500 et 2400nm )
+End.wave <- 2400
+wv <- seq(Start.wave,End.wave,1)
+Spectra <- as.matrix(full_df[,names(full_df) %in% wv])
+
+colnames(Spectra) <- c(paste0("Wave_",wv))
+head(Spectra)[1:6,1:10]
+sample_info <- full_df[,names(full_df) %notin% seq(350,2500,1)] 
+head(sample_info)
+
+
+sample_info2 <- sample_info %>%
+  select(X1001g_ID,AOP_status,Classification_name) ########## vraiment c'est données ou changer pour autre chose??
+head(sample_info2)
+
+plsr_data <- data.frame(sample_info2,Spectra) #join le sample et spectra
+
+# rm(sample_info,sample_info2,Spectra) (((pas encore sure de ca)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
