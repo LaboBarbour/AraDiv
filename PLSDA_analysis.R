079fd6724416c6d5039415bfd93a08a619aa8c0f
Sandrine L'Anglais-Landry <slanglaislandrysds@gmail.com>
2023-11-09 10:01:26 -0500

    i think it a fonctional plsda analysis

--- PLSDA_analysis.R
+++ PLSDA_analysis.R
@@ -20,34 +20,8 @@ model <- plsda(spectral_df, chemotype_vec, ncomp = 2)
 summary(model)
 plot(model)
 
-#classification plot
-plotPredictions(model)
-
-#if many classes of model
-plotPredictions(model, nc = 1)
-
-plotPredictions(model, nc = 3)
 
 
-#performance plot 
-
-par(mfrow = c(3,1))
-
-plotMisclassified(model)
-
-plotSensitivity(model)
-
-plotSpecificity(model)
-
-
-par(mfrow = c(1, 2))
-plotRegcoeffs(model, ncomp = 2, ny = 3)
-plotRegcoeffs(model, ncomp = 2, show.ci = TRUE)
-
-#predicting new data
-
-res = predict(model)
-summary(res)
 
 
 
@@ -84,12 +58,12 @@ validation_indexes = createDataPartition(full_df_clean_reduced$classification_na
                                          list = FALSE)
 
 
-# Create the validation dataset
+# Create the calibration dataset
 
 Xc <- (full_df_clean_reduced[-validation_indexes,1:2151 ]) 
 
 
-# The remaining data will be used for calibration
+# The remaining data will be used for validation
 Xv <- (full_df_clean_reduced[validation_indexes,1:2151 ]) 
 
 #those are factors and the classes you wish to predict
@@ -103,11 +77,12 @@ cv.all_factor <- as.factor(cv.all$classification_name)
 
 
 # calibrating the data
-m.all.c <- plsda(Xc, cc.all_factor, ncomp = 2, cv = 1)
+m.all.c <- plsda(Xc, cc.all_factor, ncomp = 7, cv = 1)
 
-m.all.v <- plsda(Xv, cv.all_factor, ncomp = 2, cv = 1)
+m.all.v <- plsda(Xv, cv.all_factor, ncomp = 7, cv = 1)
 
 summary(m.all.c)
+summary(m.all.v)
 
 #to look for a single component/class
 summary(m.all.c, nc = 3)
@@ -126,30 +101,53 @@ getConfusionMatrix(m.all.c$calres)
 #classification plot
 
 par(mfrow = c(1, 2))
-plotPredictions(m.all.c)
-plotPredictions(m.all.v)
+plotPredictions(m.all.c,ncomp = 7)
+plotPredictions(m.all.v, ncomp = 7)
 
 #multiple classes model you can select which class to show the predictions for.
 
 
 par(mfrow = c(1, 2))
-plotPredictions(m.all.c, nc = 1)
-plotPredictions(m.all.c, nc = 3)
+plotPredictions(m.all.c, ncomp = 6)
+plotPredictions(m.all.c, ncomp = 6)
 
-# performance plots ((((i run into an issue, why dont i have 2 lines???))))
+# performance plots 
+par(mfrow = c(2, 1))
+plotMisclassified(m.all.c, nc = 7)
+plotMisclassified(m.all.v, nc = 7)
 
-par(mfrow = c(3, 2))
-plotMisclassified(m.all.c, nc = 2)
-plotMisclassified(m.all.v, nc = 2)
+plotSensitivity(m.all.c, nc = 7)
+plotSensitivity(m.all.v, nc = 7)
 
-plotSensitivity(m.all.c, nc = 2)
-plotSensitivity(m.all.v, nc = 2)
+plotSpecificity(m.all.c, nc = 7)
+plotSpecificity(m.all.v, nc = 7)
 
-plotSpecificity(m.all.c, nc = 2)
-plotSpecificity(m.all.v, nc = 2)
+# add show.ci = TRUE at the end if you want to see the error bars
 
-plotRegcoeffs(m.all, ncomp = 3, ny = 3)
+par(mfrow = c(1, 1))
+
+plotRegcoeffs(m.all.c, ncomp = 7, ny = 1)
+plotRegcoeffs(m.all.c, ncomp = 7, ny = 2)
+plotRegcoeffs(m.all.c, ncomp = 7, ny = 3)
+plotRegcoeffs(m.all.c, ncomp = 7, ny = 4)
+plotRegcoeffs(m.all.c, ncomp = 7, ny = 5)
+plotRegcoeffs(m.all.c, ncomp = 7, ny = 6)
+plotRegcoeffs(m.all.c, ncomp = 7, ny = 7)
+
+plotRegcoeffs(m.all.v, ncomp = 7, ny = 1)
+plotRegcoeffs(m.all.v, ncomp = 7, ny = 2)
+plotRegcoeffs(m.all.v, ncomp = 7, ny = 3)
+plotRegcoeffs(m.all.v, ncomp = 7, ny = 4)
+plotRegcoeffs(m.all.v, ncomp = 7, ny = 5)
+plotRegcoeffs(m.all.v, ncomp = 7, ny = 6)
+plotRegcoeffs(m.all.v, ncomp = 7, ny = 7)
 
 
 
+# prediction for new data
+
+summary(m.all.v$calres)
+
+par(mfrow = c(1, 1))
+plotPredictions(m.all.v$calres, ncomp = 7)
 
