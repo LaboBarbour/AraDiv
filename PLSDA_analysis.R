325b25509fb5c2d3dc8524751a7547f23c158553
Sandrine L'Anglais-Landry <slanglaislandrysds@gmail.com>
2023-11-10 15:08:12 -0500

    corrections with matt + pca

--- PLSDA_analysis.R
+++ PLSDA_analysis.R
@@ -15,18 +15,14 @@ spectral_df <- select(full_df_clean, x350:x2500)
 chemotype_vec <- factor(full_df_clean$classification_name)
 
 
-## PLDA analysis
+## PLSDA analysis
 model <- plsda(spectral_df, chemotype_vec, ncomp = 2)
 summary(model)
 plot(model)
 
 
 
-
-
-
-
-# this works, i created validation and calibration model 30/70
+#i created validation and calibration model 30/70
 #this is the same analysis than the example : https://www.mdatools.com/docs/plsda--calibration.html
 # i changed the way i created the data split to make it work with my data
 
@@ -52,6 +48,27 @@ validation_proportion <- 0.3  # You can adjust this as needed
 full_df_clean_reduced <- full_df_clean %>%
   select(starts_with("x"), -x1001g_id, classification_name)
 
+
+
+#------------------------------------------------------------------------------
+#making a pca analysis to know how many comp are needed
+library(ggfortify)
+
+temp_pca <- pca(select(full_df_clean_reduced, -classification_name))
+summary(temp_pca)
+
+pca_res <- prcomp(select(full_df_clean_reduced, -classification_name))
+summary(pca_res)
+autoplot(pca_res, data = full_df_clean_reduced, color = "classification_name") +
+  scale_color_viridis_d()
+
+pca_res_scaled <- prcomp(select(full_df_clean_reduced, -classification_name), scale = T)
+autoplot(pca_res_scaled, data = full_df_clean_reduced, color = "classification_name") +
+  scale_color_viridis_d()
+#------------------------------------------------------------------------------
+
+#continuing the analysis with the ncomp in mind 
+
 # Create a random sample of data points for the validation dataset
 validation_indexes = createDataPartition(full_df_clean_reduced$classification_name, 
                                          p = validation_proportion, 
@@ -75,6 +92,8 @@ cv.all = (full_df_clean_reduced[validation_indexes, 2152])
 cc.all_factor <- as.factor(cc.all$classification_name)
 cv.all_factor <- as.factor(cv.all$classification_name)
 
+table(full_df_clean_reduced$classification_name)
+table(cc.all_factor)
 
 # calibrating the data
 m.all.c <- plsda(Xc, cc.all_factor, ncomp = 7, cv = 1)
@@ -96,16 +115,16 @@ summary(m.all.c$calres)
 summary(m.all.v$calres)
 
 getConfusionMatrix(m.all.c$calres)
+getConfusionMatrix(m.all.v$calres)
 
 
 #classification plot
 
 par(mfrow = c(1, 2))
-plotPredictions(m.all.c,ncomp = 7)
-plotPredictions(m.all.v, ncomp = 7)
+plotPredictions(m.all.c,ncomp = 3)
+plotPredictions(m.all.v, ncomp = 3 )
 
 #multiple classes model you can select which class to show the predictions for.
-
 
 par(mfrow = c(1, 2))
 plotPredictions(m.all.c, ncomp = 6)
@@ -124,7 +143,7 @@ plotSpecificity(m.all.v, nc = 7)
 
 # add show.ci = TRUE at the end if you want to see the error bars
 
-par(mfrow = c(1, 1))
+par(mfrow = c(3, 1))
 
 plotRegcoeffs(m.all.c, ncomp = 7, ny = 1)
 plotRegcoeffs(m.all.c, ncomp = 7, ny = 2)
@@ -150,4 +169,8 @@ summary(m.all.v$calres)
 
 par(mfrow = c(1, 1))
 plotPredictions(m.all.v$calres, ncomp = 7)
+
+
+res = predict(m.all.c, Xv, cv.all_factor)
+summary(res)
 
