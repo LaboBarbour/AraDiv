0336bcb36e2f371585ab3f405f1f6f5f63b20e5e
Sandrine L'Anglais-Landry <slanglaislandrysds@gmail.com>
2023-11-10 16:13:41 -0500

    mam+aop analysis

---
+++ PLSDA_AOP_status.R
@@ -0,0 +1,100 @@
+## Setup ----
+
+# load libraries
+library(tidyverse)
+library(mdatools)
+library(caret)
+
+# load data
+full_df_clean <- read_csv2("full_df_clean.csv")
+
+# Set the seed for reproducibility
+set.seed(12345)
+
+# Specify the proportion for the validation dataset
+validation_proportion <- 0.3  # You can adjust this as needed
+
+#clean up data for val/cal dataset
+
+
+full_df_clean_aop <- full_df_clean %>%
+  select(starts_with("x"), -x1001g_id, aop_status)
+
+#------------------------------------------------------------------------------
+#making a pca analysis to know how many comp are needed
+library(ggfortify)
+
+temp_pca_aop <- pca(select(full_df_clean_aop, -aop_status))
+summary(temp_pca_aop)
+
+pca_res_aop <- prcomp(select(full_df_clean_aop, -aop_status))
+summary(pca_res_aop)
+autoplot(pca_res_aop, data = full_df_clean_aop, color = "aop_status") +
+  scale_color_viridis_d()
+
+pca_res_scaled_aop <- prcomp(select(full_df_clean_aop, -aop_status), scale = T)
+autoplot(pca_res_scaled_aop, data = full_df_clean_aop, color = "aop_status") +
+  scale_color_viridis_d()
+#------------------------------------------------------------------------------
+
+#continuing the analysis with the ncomp in mind 
+
+# Create a random sample of data points for the validation dataset
+validation_indexes_aop = createDataPartition(full_df_clean_aop$aop_status, 
+                                         p = validation_proportion, 
+                                         list = FALSE)
+# Create the calibration dataset
+
+Xc_aop <- (full_df_clean_aop[-validation_indexes_aop,1:2151 ]) 
+
+
+# The remaining data will be used for validation
+Xv_aop <- (full_df_clean_aop[validation_indexes_aop,1:2151 ]) 
+
+#those are factors and the classes you wish to predict
+
+cc.all_aop = (full_df_clean_aop[-validation_indexes_aop, 2152])
+cv.all_aop = (full_df_clean_aop[validation_indexes_aop, 2152])
+
+#transform in factors
+cc.all_aop_factor <- as.factor(cc.all_aop$aop_status)
+cv.all_aop_factor <- as.factor(cv.all_aop$aop_status)
+
+#to make sure its 30/70
+table(full_df_clean_aop$aop_status)
+table(cc.all_aop_factor)
+
+# calibrating the data
+m.all.c_aop <- plsda(Xc_aop, cc.all_aop_factor, ncomp = 2, cv = 1)
+
+summary(m.all.c_aop)
+
+
+#to look for a single component/class
+summary(m.all.c_aop, nc = 3)
+
+#show statistics only for calibration or only for cross-validation parts, 
+#in this case you will see details about contribution of every component 
+#to the model.
+
+summary(m.all.c_aop$calres)
+
+
+getConfusionMatrix(m.all.c_aop$calres)
+
+
+#classification plot
+
+par(mfrow = c(1, 2))
+plotPredictions(m.all.c_aop,ncomp = 3)
+
+
+
+
+
+
+
+
+
+
+
---
+++ PLSDA_mam_status.R
@@ -0,0 +1,91 @@
+## Setup ----
+
+# load libraries
+library(tidyverse)
+library(mdatools)
+library(caret)
+
+# load data
+full_df_clean <- read_csv2("full_df_clean.csv")
+
+# Set the seed for reproducibility
+set.seed(12345)
+
+# Specify the proportion for the validation dataset
+validation_proportion <- 0.3  # You can adjust this as needed
+
+#clean up data for val/cal dataset
+
+
+full_df_clean_mam <- full_df_clean %>%
+  select(starts_with("x"), -x1001g_id, mam_status)
+
+#------------------------------------------------------------------------------
+#making a pca analysis to know how many comp are needed
+library(ggfortify)
+
+temp_pca_mam <- pca(select(full_df_clean_mam, -mam_status))
+summary(temp_pca_mam)
+
+pca_res_mam <- prcomp(select(full_df_clean_mam, -mam_status))
+summary(pca_res_mam)
+autoplot(pca_res_mam, data = full_df_clean_mam, color = "mam_status") +
+  scale_color_viridis_d()
+
+pca_res_scaled_mam <- prcomp(select(full_df_clean_mam, -mam_status), scale = T)
+autoplot(pca_res_scaled_mam, data = full_df_clean_mam, color = "mam_status") +
+  scale_color_viridis_d()
+#------------------------------------------------------------------------------
+
+#continuing the analysis with the ncomp in mind 
+
+# Create a random sample of data points for the validation dataset
+validation_indexes_mam = createDataPartition(full_df_clean_mam$mam_status, 
+                                             p = validation_proportion, 
+                                             list = FALSE)
+# Create the calibration dataset
+
+Xc_mam <- (full_df_clean_mam[-validation_indexes_mam,1:2151 ]) 
+
+
+# The remaining data will be used for validation
+Xv_mam <- (full_df_clean_mam[validation_indexes_mam,1:2151 ]) 
+
+#those are factors and the classes you wish to predict
+
+cc.all_mam = (full_df_clean_mam[-validation_indexes_mam, 2152])
+cv.all_mam = (full_df_clean_mam[validation_indexes_mam, 2152])
+
+#transform in factors
+cc.all_mam_factor <- as.factor(cc.all_mam$mam_status)
+cv.all_mam_factor <- as.factor(cv.all_mam$mam_status)
+
+#to make sure its 30/70
+table(full_df_clean_mam$mam_status)
+table(cc.all_mam_factor)
+
+# calibrating the data
+m.all.c_mam <- plsda(Xc_mam, cc.all_mam_factor, ncomp = 2, cv = 1)
+
+summary(m.all.c_mam)
+
+
+#to look for a single component/class
+summary(m.all.c_mam, nc = 3)
+
+#show statistics only for calibration or only for cross-validation parts, 
+#in this case you will see details about contribution of every component 
+#to the model.
+
+summary(m.all.c_mam$calres)
+
+
+getConfusionMatrix(m.all.c_mam$calres)
+
+
+#classification plot
+
+par(mfrow = c(1, 2))
+plotPredictions(m.all.c_mam,ncomp = 3)
+
+
